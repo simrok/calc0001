2e2eed1e48d28cdcbd7364556d93e6276f9699a9
simrok <162396191+simrok@users.noreply.github.com>
2024-04-10 16:27:24 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name = 'yunasim2024',
+    version = '0.0.1',
+    description = 'nice calculator'
+    author = 'simrok',
+    author_email = 'ynna3784@gmail.com',
+    url = 'https://github.com/simrok/calc0001',
+    packages = ['mycalc'],
+    classifiers = [
+        "Programming Language :: Python ;; 3"
+    ]
+)
